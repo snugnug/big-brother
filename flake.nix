fed4c8607c7ee5663312470a52842f440fb85868
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-12-02 16:07:26 +0400

    init init

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Big Brother Nix Flake";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+      inherit (builtins) attrValues;
+      eachSystem = f:
+        lib.genAttrs [ "x86_64-linux" ]
+        (system: f nixpkgs.legacyPackages.${system});
+    in {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages =
+            attrValues { inherit (pkgs) cargo rustc rust-analyzer rustfmt; };
+        };
+      });
+    };
+}
