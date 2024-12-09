81a021d480ac9dcd65fc0488469f8bc2f5819f80
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-12-09 15:56:15 +0400

    getting somewhere

--- src/github.rs
+++ src/github.rs
@@ -1,18 +1,25 @@
 use reqwest::Client;
+use serde::{Deserialize, Serialize};
 use std::collections::hash_map::HashMap;
 
-pub async fn get_pr_info(pr: u32) -> Result<(), Box<dyn std::error::Error>> {
+#[derive(Deserialize, Serialize, Debug)]
+pub struct PrInfo {
+    // Holy shit there are so many prs on nixpkgs
+    number: u64,
+    title: String,
+}
+
+pub async fn get_pr_info(pr: u32) -> Result<PrInfo, Box<dyn std::error::Error>> {
     let client = reqwest::Client::builder()
         .user_agent(format!("big-brother {}", env!("CARGO_PKG_VERSION")))
         .build()?;
     
-    let response = client.get(format!("https://api.github.com/repos/nixos/nixpkgs/pulls/{}", pr))
+    let response: PrInfo = client.get(format!("https://api.github.com/repos/nixos/nixpkgs/pulls/{}", pr))
         .send()
         .await?
-	.json::<serde_json::Value>()
+	.json::<PrInfo>()
 	.await?;
 
     // println!("{response:?}");
-    println!("{}", serde_json::to_string_pretty(&response).unwrap());
-    Ok(())
+    Ok(response)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-mod github;
+use reqwest::Response;
 
+mod github;
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
-    github::get_pr_info(32).await;
+    // github::get_pr_info(32).await;
+    let test = github::get_pr_info(32).await;
+
+    println!("{:?}", test);
  }
