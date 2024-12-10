f60fabf1969380cc82faeae1060660b33b3438b5
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-12-10 08:41:17 +0400

    cleanup

--- src/github.rs
+++ src/github.rs
@@ -1,24 +1,15 @@
 use reqwest::Client;
-use serde::{Deserialize, Serialize};
-use std::collections::hash_map::HashMap;
+use serde_json::Value;
 
-#[derive(Deserialize, Serialize, Debug)]
-pub struct PrInfo {
-    // Holy shit there are so many prs on nixpkgs
-    number: u64,
-    state: String,
-    title: String,
-}
-
-pub async fn get_pr_info(pr: u32) -> Result<serde_json::value::Value, Box<dyn std::error::Error>> {
-    let client = reqwest::Client::builder()
+pub async fn get_pr_info(pr: u32) -> Result<Value, Box<dyn std::error::Error>> {
+    let client = Client::builder()
         .user_agent(format!("big-brother {}", env!("CARGO_PKG_VERSION")))
         .build()?;
     
-    let response: serde_json::value::Value = client.get(format!("https://api.github.com/repos/nixos/nixpkgs/pulls/{}", pr))
+    let response: Value = client.get(format!("https://api.github.com/repos/nixos/nixpkgs/pulls/{}", pr))
         .send()
         .await?
-	.json::<serde_json::value::Value>()
+	.json::<Value>()
 	.await?;
 
     // println!("{response:?}");
