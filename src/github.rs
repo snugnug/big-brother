00535986cf229862ab12fe2090727efe113b27f7
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-12-10 10:32:45 +0400

    it worked

--- src/github.rs
+++ src/github.rs
@@ -1,15 +1,21 @@
 use reqwest::Client;
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
-pub async fn get_pr_info(pr: u32) -> Result<Value, Box<dyn std::error::Error>> {
+#[derive(Serialize, Deserialize)]
+pub struct PrInfo {
+    pub state: String,
+}
+
+pub async fn get_pr_info(pr: u32) -> Result<PrInfo, Box<dyn std::error::Error>> {
     let client = Client::builder()
         .user_agent(format!("big-brother {}", env!("CARGO_PKG_VERSION")))
         .build()?;
     
-    let response: Value = client.get(format!("https://api.github.com/repos/nixos/nixpkgs/pulls/{}", pr))
+    let response: PrInfo = client.get(format!("https://api.github.com/repos/nixos/nixpkgs/pulls/{}", pr))
         .send()
         .await?
-	.json::<Value>()
+	.json::<PrInfo>()
 	.await?;
 
     // println!("{response:?}");
