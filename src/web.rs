9ca64183f1bafec909160af4108889c20ee74797
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-01-21 17:39:39 +0400

    too lazy

--- src/web.rs
+++ src/web.rs
@@ -1,12 +1,21 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, response::Html, Router};
+use askama::{Template};
 
 pub async fn serve_web() {
     serve().await
 }
 
+#[derive(Template)]
+#[template(path = "pr.html")]
+struct Test {}
+
 async fn serve() {
+
+    let template = Test {};
+    
     let app = Router::new()
-        .route("/", get("Hi"));;
+        .route("/pr", get(Html(template.render().unwrap())))
+        .route("/", get("Hi"));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
