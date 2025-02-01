98b2af9586a94c27e6353f6dfe296cb14d0d7566
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-02-02 00:20:38 +0400

    make package build from tag

--- nix/package.nix
+++ nix/package.nix
@@ -1,29 +1,40 @@
 {
   lib,
   rustPlatform,
+  fetchFromGitHub,
   pkg-config,
   openssl,
-}:
-rustPlatform.buildRustPackage {
-  pname = "big-brother";
+}: let
   version = "1.0.0";
+in
+  rustPlatform.buildRustPackage {
+    pname = "big-brother";
+    version = version;
 
-  src = ../.;
+    # src = ../.;
+    # I dont know if theres a better way to do this
+    src = fetchFromGitHub {
+      owner = "snugnug";
+      repo = "big-brother";
+      tag = "v${version}";
+      # hash = lib.fakeHash;
+      hash = "sha256-iJF2fIcTcUGpDlMV5SBx9zBUFVeCETtfabuuJTqSDz8=";
+    };
 
-  nativeBuildInputs = [
-    pkg-config
-  ];
+    nativeBuildInputs = [
+      pkg-config
+    ];
 
-  buildInputs = [
-    openssl
-  ];
+    buildInputs = [
+      openssl
+    ];
 
-  # cargoHash = lib.fakeHash;
-  cargoHash = "sha256-8dZmCK0d15yf1mOzEmLhFL6XGmsabMn7bVfj5wS1rEM=";
+    # cargoHash = lib.fakeHash;
+    cargoHash = "sha256-8dZmCK0d15yf1mOzEmLhFL6XGmsabMn7bVfj5wS1rEM=";
 
-  meta = {
-    description = "A nixpkgs tracker with notifications!";
-    homepage = "https://github.com/snugnug/big-brother";
-    license = lib.licenses.gpl3;
-  };
-}
+    meta = {
+      description = "A nixpkgs tracker with notifications!";
+      homepage = "https://github.com/snugnug/big-brother";
+      license = lib.licenses.gpl3;
+    };
+  }
