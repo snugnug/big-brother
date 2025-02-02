93a7436e2c97c1e4d1589a4b07518f50366b4ed1
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-02-02 09:10:29 +0400

    fix the broken fix

--- src/web.rs
+++ src/web.rs
@@ -62,70 +62,71 @@ async fn get_pr(Path(pr_id): Path<u64>) -> Html<String> {
             branches: vec![],
             merged_into: vec![],
         };
+
         return Html(template.render().unwrap());
-    }
+    } else {
+        let target_branches = vec![
+            "master".to_string(),
+            "nixpkgs-unstable".to_string(),
+            "nixos-unstable-small".to_string(),
+            "nixos-unstable".to_string(),
+            "nixos-24.11-small".to_string(),
+            "nixos-24.11".to_string(),
+        ];
 
-    let target_branches = vec![
-        "master".to_string(),
-        "nixpkgs-unstable".to_string(),
-        "nixos-unstable-small".to_string(),
-        "nixos-unstable".to_string(),
-        "nixos-24.11-small".to_string(),
-        "nixos-24.11".to_string(),
-    ];
+        let mut in_branches: Vec<bool> = vec![];
 
-    let mut in_branches: Vec<bool> = vec![];
-
-    let tasks: Vec<_> = target_branches
-        .clone()
-        .iter()
-        .map(|branch| {
-            let branch_clone = branch.clone();
-            let client_clone = client.clone();
-            let pr_merge_commit_sha = pr.merge_commit_sha.clone();
-            tokio::spawn(async move {
-                github::compare_branches_api(
-                    client_clone,
-                    branch_clone,
-                    pr_merge_commit_sha.unwrap(),
-                )
-                .await
+        let tasks: Vec<_> = target_branches
+            .clone()
+            .iter()
+            .map(|branch| {
+                let branch_clone = branch.clone();
+                let client_clone = client.clone();
+                let pr_merge_commit_sha = pr.merge_commit_sha.clone();
+                tokio::spawn(async move {
+                    github::compare_branches_api(
+                        client_clone,
+                        branch_clone,
+                        pr_merge_commit_sha.unwrap(),
+                    )
+                    .await
+                })
             })
-        })
-        .collect();
+            .collect();
 
-    for task in tasks {
-        match task.await {
-            Ok(data) => in_branches.push(data.unwrap()),
-            Err(err) => {
-                tracing::error!("Failed to get pr, {}", err);
-                let template = PullRequest {
-                    pr_title: "Errored!".to_string(),
-                    failed: true,
-                    closed: false,
-                    error: err.to_string(),
-                    branches: vec![],
-                    merged_into: vec![],
-                };
-                return Html(template.render().unwrap());
-            }
+        for task in tasks {
+            match task.await {
+                Ok(data) => in_branches.push(data.unwrap()),
+                Err(err) => {
+                    tracing::error!("Failed to get pr, {}", err);
+                    let template = PullRequest {
+                        pr_title: "Errored!".to_string(),
+                        failed: true,
+                        closed: false,
+                        error: err.to_string(),
+                        branches: vec![],
+                        merged_into: vec![],
+                    };
+                    return Html(template.render().unwrap());
+                }
+            };
+        }
+
+        if in_branches.is_empty() || in_branches.is_empty() {
+            tracing::warn!("Results or in_branches are empty, check logic.");
+        }
+
+        let template = PullRequest {
+            pr_title: pr.title,
+            failed: false,
+            closed: false,
+            error: "You shouldn't see this lol".to_string(),
+            branches: target_branches,
+            merged_into: in_branches,
         };
-    }
 
-    if in_branches.is_empty() || in_branches.is_empty() {
-        tracing::warn!("Results or in_branches are empty, check logic.");
-    }
-
-    let template = PullRequest {
-        pr_title: pr.title,
-        failed: false,
-        closed: false,
-        error: "You shouldn't see this lol".to_string(),
-        branches: target_branches,
-        merged_into: in_branches,
+        return Html(template.render().unwrap());
     };
-
-    return Html(template.render().unwrap());
 }
 
 async fn index() -> Html<String> {
